f292b62df052eafa0599d61aa808dd8920bbead2
Aducj1910 <adish1910@gmail.com>
2020-12-11 12:29:59 +0530

    hide elixir display

---
+++ Assets/Scripts/ShowHideElixirDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideElixirDisplay : MonoBehaviour
+{
+    public GameObject elixirDisplay;
+    bool state = false;
+
+
+    public void SwitchShowHide()
+    {
+        state = !state;
+        elixirDisplay.gameObject.SetActive(state);
+    }
+}
