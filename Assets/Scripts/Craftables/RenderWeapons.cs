ec110ccfb4497f68b2ff93117f0ea2b604bae357
akritij2 <akritij2@illinois.edu>
2020-12-13 12:09:46 +0530

    Clickable weapons in crafting menu

--- Assets/Scripts/Craftables/RenderWeapons.cs
+++ Assets/Scripts/Craftables/RenderWeapons.cs
@@ -10,8 +10,6 @@ public class RenderWeapons : MonoBehaviour
     void Start()
     {
         GameObject weaponTemplate = transform.GetChild(0).gameObject;
-        GameObject weaponsManager = GameObject.Find("WeaponsManager");
-
         Weapon[] weapons = FindObjectOfType<WeaponsManager>().weapons;
 
         //Get elixir values 
@@ -33,19 +31,32 @@ public class RenderWeapons : MonoBehaviour
             GameObject gElixirCost = gButton.transform.GetChild(1).gameObject;
 
             int[] costs = weapon.getElixirCosts();
-
-            for (int i = 0; i <4; i++)
+            bool canCraft = true;
+            for (int i = 0; i < 4; i++)
             {
                 GameObject gCost = gElixirCost.transform.GetChild(i).gameObject;
                 GameObject gCostText = gCost.transform.GetChild(0).gameObject;
                 gCostText.GetComponent<TMPro.TextMeshProUGUI>().text = costs[i].ToString();
                 if (costs[i] > elixirsArray[i])
                 {
+                    canCraft = false;
                     gCostText.GetComponent<TMPro.TextMeshProUGUI>().color = new Color32(255, 0, 0, 255);
                 }
             }
-            
 
+            gButton.GetComponent<Button>().onClick.AddListener(delegate ()
+            {
+                //Debug.Log("Clicked " + weapon.name);
+                if (canCraft)
+                {
+                    GameObject inventoryManager = GameObject.Find("InventoryManager");
+                    inventoryManager.GetComponent<InventoryManager>().addWeapon(weapon);
+                }
+                else
+                {
+                    Debug.Log("Can't Craft");
+                }
+            });
         }
 
         Destroy(weaponTemplate);
---
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    private static Hashtable weapons;
+    private static Hashtable tools;
+
+    public void addWeapon(Weapon w)
+    {
+        if (weapons.ContainsKey(w))
+        {
+            weapons[w] = (int)(weapons[w]) + 1;
+        }
+        else
+        {
+            weapons[w] = 1;
+        }
+        //Debug.Log(weapons[w]);
+        //Debug.Log(w.name);
+    }
+
+    public Hashtable getWeapons()
+    {
+        return weapons;
+    }
+
+    InventoryManager()
+    {
+        weapons = new Hashtable();
+        tools = new Hashtable();
+    }
+}
