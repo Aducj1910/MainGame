8d4f62be1972ef492e16996c3035e9db90b80aec
Aducj1910 <adish1910@gmail.com>
2020-12-16 23:05:17 +0530

    added the colour

--- Assets/Scripts/FightMechanics/FightBarController.cs
+++ Assets/Scripts/FightMechanics/FightBarController.cs
@@ -15,6 +15,21 @@ public class FightBarController : MonoBehaviour
     private float barHeight;
     private float barY;
 
+    //here we define the regions of the bar
+    float topRedUpper;
+    float topRedLower;
+
+    float bottomRedUpper;
+    float bottomRedLower;
+
+    float topYellowUpper;
+    float topYellowLower;
+    float bottomYellowUpper;
+    float bottomYellowLower;
+
+    float greenUpper;
+    float greenLower;
+
     void Awake()
     {
         isMoving = true;
@@ -23,6 +38,28 @@ public class FightBarController : MonoBehaviour
 
         barHeight = fightBarImg.GetComponent<RectTransform>().rect.width; //it's rotated so width here refers to height
         barY = fightBarImg.transform.position.y;
+
+        //Here we define some variables for ease of use in defining colour region floats
+        var halfBarHeight = barHeight / 2;
+        var halfBarHeightUpper = (barHeight / 2) + 0.1f;
+        var halfBarHeightLower = (barHeight / 2) - 0.1f;
+
+        //Here we define the float values of the regions using the barHeight and barY
+        topRedUpper = barY + halfBarHeight;
+        topRedLower = barY + (halfBarHeightUpper - (0.3f * barHeight));
+
+        topYellowUpper = barY + (halfBarHeightLower - (0.3f * barHeight));
+        topYellowLower = barY + (halfBarHeightUpper - (0.45f * barHeight));
+
+        greenUpper = barY + (halfBarHeightLower - (0.45f * barHeight));
+        greenLower = barY + ((-halfBarHeightUpper) + (0.45f * barHeight));
+
+        bottomYellowUpper = barY + ((-halfBarHeightLower) + (0.45f * barHeight));
+        bottomYellowLower = barY + ((-halfBarHeightUpper) + (0.3f * barHeight));
+
+        bottomRedUpper = barY + ((-halfBarHeightLower) + (0.3f * barHeight));
+        bottomRedLower = barY - halfBarHeight; 
+
     }
 
     void Update()
@@ -30,7 +67,18 @@ public class FightBarController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             isMoving = false;
-            Debug.Log(transform.position.y);
+            if ((transform.position.y > topRedLower && transform.position.y < topRedUpper) || (transform.position.y > bottomRedLower && transform.position.y < bottomRedUpper))
+            {
+                Debug.Log("Red");
+            }
+            else if ((transform.position.y > topYellowLower && transform.position.y < topYellowUpper) || (transform.position.y > bottomYellowLower && transform.position.y < bottomYellowUpper))
+            {
+                Debug.Log("Yellow");
+            }
+            else if(transform.position.y > greenLower && transform.position.y < greenUpper)
+            {
+                Debug.Log("Green");
+            }
         }
 
         if (isMoving)
