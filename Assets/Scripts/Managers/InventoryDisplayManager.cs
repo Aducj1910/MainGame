6b5bae6ad1017fbdb798bd7bf3e2e50948e35b16
Aducj1910 <adish1910@gmail.com>
2020-12-21 14:25:16 +0530

    canvas inventory tools

--- Assets/Scripts/Managers/InventoryDisplayManager.cs
+++ Assets/Scripts/Managers/InventoryDisplayManager.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 public class InventoryDisplayManager : MonoBehaviour //for ui inventory behaviour
 {
     [HideInInspector] public GameObject UI_Inv;
-    [HideInInspector] public bool currentSetActiveState;
+    private bool currentSetActiveState;
+    [HideInInspector] public GameObject UI_Inv2;
+    private bool currentSetActiveState2;
 
 
     void Start()
     {
         UI_Inv = GameObject.Find("UI_Inventory");
         currentSetActiveState = false;
+
+        UI_Inv2 = GameObject.Find("UI_Inventory2");
+        currentSetActiveState2 = false; 
     }
 
     void Update()
     { 
         UI_Inv.SetActive(currentSetActiveState);
+        UI_Inv2.SetActive(currentSetActiveState2);
 
-
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.W))
         {
             UI_Inv.GetComponent<UI_Inventory>().refreshInventorySlots();
             currentSetActiveState = !currentSetActiveState;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            UI_Inv2.GetComponent<UI_Inventory2>().refreshInventorySlots();
+            currentSetActiveState2 = !currentSetActiveState2;
         }
     }
 }
---
+++ Assets/Scripts/Managers/UI_Inventory2.cs
@@ -0,0 +1,64 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Inventory2 : MonoBehaviour
+{
+    private List<Tool> tools;
+    private GameObject inventoryManager;
+    private Component invManager;
+
+    private Transform itemSlotContainer;
+    private Transform itemSlotTemplate;
+
+    public bool inventoryActive;
+
+    void Awake()
+    {
+        itemSlotContainer = transform.Find("ItemSlotContainer");
+        itemSlotTemplate = itemSlotContainer.Find("ItemSlotTemplate");
+    }
+
+    void Start()
+    {
+        inventoryManager = GameObject.Find("InventoryManager");
+    }
+
+    public void refreshInventorySlots()
+    {
+        int x = 0;
+        int y = 0;
+        float itemSlotCellSize = 100f;
+        tools = inventoryManager.GetComponent<InventoryManager>().getTools();
+
+        if (tools != null)
+        {
+            foreach (var tool in tools)
+            {
+                RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+                itemSlotRectTransform.gameObject.SetActive(true);
+                itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
+
+                itemSlotRectTransform.GetComponent<Button_UI>().ClickFunc = () =>
+                {
+                    inventoryManager.GetComponent<InventoryManager>().setCurrentTool(tool);
+                };
+
+                Image image = itemSlotRectTransform.Find("Image").GetComponent<Image>();
+                image.sprite = tool.assetImage;
+
+                x++;
+                if (x > 3)
+                {
+                    x = 0;
+                    y--;
+                }
+
+            }
+        }
+
+    }
+    
+}
